4f798bacaaa3f391b822653fb5389dae1759ec87
zhuyaqun <2531110838@qq.com>
2024-09-18 16:06:26 +0800

    修改Main.java3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome!");
+        System.out.println("Hello World!");
     }
 }
