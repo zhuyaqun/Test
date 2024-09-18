6b92e539d5021daac42a04b9e19c2b790ba10049
zhuyaqun <2531110838@qq.com>
2024-09-18 16:00:16 +0800

    修改Main.java2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ package org.example;
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.println("Hello and welcome!");
+        System.out.println("Welcome!");
     }
 }
