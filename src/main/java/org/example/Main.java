f6911dcf708fe7cf6a85d6d8f5274ef1ffac8706
zhuyaqun <2531110838@qq.com>
2024-09-18 15:09:44 +0800

    修改Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.println("Hello and welcome!");
+        System.out.println("Welcome!");
     }
 }
